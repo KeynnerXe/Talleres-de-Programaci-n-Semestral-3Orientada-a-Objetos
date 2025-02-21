4b7f6439e73a95b2bf0af9e4df0a33e216abfd30
Keinner Ramos <keinner.ramoszuniga@unicolombo.edu.co>
2025-02-21 08:28:36 -0500

    Add files via upload

---
+++ Installer crack/Script.bat
@@ -0,0 +1,6 @@
+@echo off
+set "JAVA_HOME=C:\Users\PC06-306\Desktop\Netbeans crack\Jdk\jdk-23"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+javac -version
+start "" "C:\Users\PC06-306\Desktop\bloodstrike_global_1.003.650003.1736933117.exe"
+pause
